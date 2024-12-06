965d73af6332a01da998a784819630d6a5dc18a1
Lilith Isenberg <liliththana@gmail.com>
2024-12-06 08:26:31 -0600

    adds DefaultState.cs script template

---
+++ script_templates/State/DefaultState.cs
@@ -0,0 +1,28 @@
+// meta-name: Default State
+// meta-description: State template with all overridable methods
+// meta-default: true
+
+using _BINDINGS_NAMESPACE_;
+
+public partial class _CLASS_ : _BASE_
+{
+    public override void Enter()
+    {
+    }
+
+    public override void HandleInput(InputEvent @event)
+    {
+    }
+
+    public override void Update(double delta)
+    {
+    }
+
+    public override void UpdatePhysics(double delta)
+    {
+    }
+
+    public override void Exit()
+    {
+    }
+}
